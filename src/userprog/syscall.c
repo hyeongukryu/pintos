714e82acb97310409e3b77ef6999557c50a6c80c
류형욱 <mintrupt@mintrupt.com>
2014-09-29 14:52:39 +0900

    매크로를 인라인 함수로 바꿉니다.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -19,7 +19,7 @@ static bool create (const char *file, unsigned initial_size);
 static bool remove (const char *file);
 
 void
-syscall_init (void) 
+syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
@@ -74,47 +74,52 @@ get_user_string (const char *str)
 }
 
 // 플래그가 맞을 때 동적 할당된 문자열을 해제하고 널 포인터를 대입합니다.
-#define free_single_user_string(args, flag, index) do { \
-  if (flag & (0b1000 >> index)) \
-  { \
-    free (args[index]); \
-    args[index] = 0; \
-  } \
-} while (0)
+static inline void
+free_single_user_string (char **args, int flag, int index)
+{
+  if (flag & (0b1000 >> index))
+    {
+      free (args[index]);
+      args[index] = 0;
+    }
+}
 
 // 플래그의 마지막 4비트 설정에 따라서 문자열들을 해제합니다.
 static inline void
 free_user_strings (char **args, int flag)
 {
   ASSERT (0 <= flag && flag <= 0b1111);
-  free_single_user_string(args, flag, 0);
-  free_single_user_string(args, flag, 1);
-  free_single_user_string(args, flag, 2);
-  free_single_user_string(args, flag, 3);
+  free_single_user_string (args, flag, 0);
+  free_single_user_string (args, flag, 1);
+  free_single_user_string (args, flag, 2);
+  free_single_user_string (args, flag, 3);
 }
 
 // 플래그에 맞을 때 사용자 문자열을 복사합니다.
 // 작업 중 실패하면 내용을 되돌리고 종료합니다.
 // 유효성을 검증한 다음 이 작업을 실행해야 합니다.
-#define get_single_user_string(args, flag, index) do { \
-  if (flag & (0b1000 >> index)) \
-  { \
-    args[index] = get_user_string (args[index]); \
-    if (!args[index]) \
-    { \
-      free_user_strings (args, flag & (0b11110000 >> index)); \
-      exit(-1); \
-    } \
-  } \
-} while (0)
+static inline void
+get_single_user_string (char **args, int flag, int index)
+{
+  if (flag & (0b1000 >> index))
+    {
+      args[index] = get_user_string (args[index]);
+      if (!args[index])
+        {
+          free_user_strings (args, flag & (0b11110000 >> index));
+          exit(-1);
+        }
+    }
+}
 
 // 플래그가 맞을 때 사용자 문자열의 유효성을 확인합니다.
 // 유효하지 않으면 종료합니다.
-#define check_single_user_string(args, flag, index) do { \
-  if (flag & (0b1000 >> index)) { \
-    check_user_string (args[index]); \
-  } \
-} while (0)
+static inline void
+check_single_user_string (char **args, int flag, int index)
+{
+  if (flag & (0b1000 >> index))
+    check_user_string (args[index]);
+}
 
 // 플래그의 마지막 4비트에 따라서 사용자 문자열을 확인하고 가져옵니다.
 // 새로운 메모리를 동적 할당합니다. 
@@ -122,65 +127,65 @@ static inline void
 get_user_strings (char **args, int flag)
 {
   ASSERT (0 <= flag && flag <= 0b1111);
-  check_single_user_string(args, flag, 0);
-  check_single_user_string(args, flag, 1);
-  check_single_user_string(args, flag, 2);
-  check_single_user_string(args, flag, 3);
-  get_single_user_string(args, flag, 0);
-  get_single_user_string(args, flag, 1);
-  get_single_user_string(args, flag, 2);
-  get_single_user_string(args, flag, 3);
+  check_single_user_string (args, flag, 0);
+  check_single_user_string (args, flag, 1);
+  check_single_user_string (args, flag, 2);
+  check_single_user_string (args, flag, 3);
+  get_single_user_string (args, flag, 0);
+  get_single_user_string (args, flag, 1);
+  get_single_user_string (args, flag, 2);
+  get_single_user_string (args, flag, 3);
 }
 
 static void
-syscall_handler (struct intr_frame *f) 
+syscall_handler (struct intr_frame *f)
 {
   int32_t args[4];
   check_address4(f->esp);
 
   switch (*(int *)f->esp)
-  {
-    case SYS_HALT:
-      halt ();
-      break;
-    case SYS_EXIT:
-      get_arguments (f->esp, args, 1);
-      exit (args[0]);
-      break;
-    case SYS_CREATE:
-      get_arguments (f->esp, args, 2);
-      get_user_strings ((char **)args, 0b1000);
-      f->eax = create ((const char *)args[0], args[1]);
-	  free_user_strings ((char **)args, 0b1000);
-      break;
-    case SYS_REMOVE:
-      get_arguments (f->esp, args, 1);
-      get_user_strings ((char **)args, 0b1000);
-      f->eax = remove ((const char *)args[0]);
-      free_user_strings ((char **)args, 0b1000); 
-      break;
-    case SYS_EXEC:
-    case SYS_WAIT:
-    case SYS_OPEN:
-    case SYS_FILESIZE:
-    case SYS_READ:
-    case SYS_WRITE:
-    case SYS_SEEK:
-    case SYS_TELL:
-    case SYS_CLOSE:
-    case SYS_MMAP:
-    case SYS_MUNMAP:
-    case SYS_CHDIR:
-    case SYS_MKDIR:
-    case SYS_READDIR:
-    case SYS_ISDIR: 
-    case SYS_INUMBER:
-      // 시스템 콜 번호는 유효하나 아직 구현되지 않았습니다.
-      printf("NotImplemented: %d\n", *(int *)f->esp);
-    default:
-      // 시스템 콜 번호가 유효하지 않습니다.
-      exit(-1);
-  }
+    {
+      case SYS_HALT:
+        halt ();
+        break;
+      case SYS_EXIT:
+        get_arguments (f->esp, args, 1);
+        exit (args[0]);
+        break;
+      case SYS_CREATE:
+        get_arguments (f->esp, args, 2);
+        get_user_strings ((char **)args, 0b1000);
+        f->eax = create ((const char *)args[0], args[1]);
+  	  free_user_strings ((char **)args, 0b1000);
+        break;
+      case SYS_REMOVE:
+        get_arguments (f->esp, args, 1);
+        get_user_strings ((char **)args, 0b1000);
+        f->eax = remove ((const char *)args[0]);
+        free_user_strings ((char **)args, 0b1000); 
+        break;
+      case SYS_EXEC:
+      case SYS_WAIT:
+      case SYS_OPEN:
+      case SYS_FILESIZE:
+      case SYS_READ:
+      case SYS_WRITE:
+      case SYS_SEEK:
+      case SYS_TELL:
+      case SYS_CLOSE:
+      case SYS_MMAP:
+      case SYS_MUNMAP:
+      case SYS_CHDIR:
+      case SYS_MKDIR:
+      case SYS_READDIR:
+      case SYS_ISDIR: 
+      case SYS_INUMBER:
+        // 시스템 콜 번호는 유효하나 아직 구현되지 않았습니다.
+        printf("NotImplemented: %d\n", *(int *)f->esp);
+      default:
+        // 시스템 콜 번호가 유효하지 않습니다.
+        exit(-1);
+    }
 }
 
 static void
@@ -208,4 +213,3 @@ remove (const char *file)
 {
   return filesys_remove (file);
 }
-
