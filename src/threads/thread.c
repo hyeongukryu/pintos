ac353121f33426dc5e60b5a374619d887b7870f6
류형욱 <mintrupt@mintrupt.com>
2014-11-05 04:32:57 +0900

    7 통과

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -77,6 +77,9 @@ static void *alloc_frame (struct thread *, size_t size);
 static void schedule (void);
 void thread_schedule_tail (struct thread *prev);
 static tid_t allocate_tid (void);
+static void test_max_priority (void);
+static bool thread_compare_priority (const struct list_elem *,
+                                     const struct list_elem *, void *);
 
 /* Initializes the threading system by transforming the code
    that's currently running into a thread.  This can't work in
@@ -234,6 +237,8 @@ thread_create (const char *name, int priority,
   /* Add to run queue. */
   thread_unblock (t);
 
+  test_max_priority ();
+
   return tid;
 }
 
@@ -289,7 +294,7 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
-  list_push_back (&ready_list, &t->elem);
+  list_insert_ordered (&ready_list, &t->elem, thread_compare_priority, 0);
   t->status = THREAD_READY;
   intr_set_level (old_level);
 }
@@ -454,7 +459,7 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (cur != idle_thread) 
-    list_push_back (&ready_list, &cur->elem);
+    list_insert_ordered (&ready_list, &cur->elem, thread_compare_priority, 0);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
@@ -482,6 +487,7 @@ void
 thread_set_priority (int new_priority) 
 {
   thread_current ()->priority = new_priority;
+  test_max_priority ();
 }
 
 /* Returns the current thread's priority. */
@@ -489,6 +495,26 @@ int
 thread_get_priority (void) 
 {
   return thread_current ()->priority;
+}
+
+static bool
+thread_compare_priority (const struct list_elem *a,
+                         const struct list_elem *b,
+                         void *aux UNUSED)
+{
+  return list_entry (a, struct thread, elem)->priority
+       > list_entry (b, struct thread, elem)->priority;  
+}
+
+static void
+test_max_priority (void)
+{
+  if (!list_empty (&ready_list) &&
+      thread_current ()->priority <
+      list_entry (list_front (&ready_list), struct thread, elem)->priority)
+    {
+      thread_yield ();
+    }
 }
 
 /* Sets the current thread's nice value to NICE. */
